f953977b281fa191f4e9df6e0657272864a1490b
Artur Sant'Anna <37422060+Artursantanna@users.noreply.github.com>
2025-06-13 00:55:13 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Driver;
+using MongoDataAccess.DataAccess;
+using MongoDataAccess.Models;
+
+ClienteDataAccess db = new ClienteDataAccess();
+
+await db.adicionar(cliente: new ClienteModel() { Nome = "Rafael Cordeiro", Email = "rafael.cordeiro@email.com.br" });
+
+var users = await db.ObterTodos();
+foreach (var cliente in users)
+{
+    Console.WriteLine($"ID: {cliente.Id}, Nome: {cliente.Nome}, Email: {cliente.Email}");
+}
